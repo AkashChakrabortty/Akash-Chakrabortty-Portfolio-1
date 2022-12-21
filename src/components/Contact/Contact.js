f016514992556f736eec122d4cafa945fc0228d0
AkashChakrabortty <akashchakrabortty@gmail.com>
2022-12-21 23:09:45 +0600

    add email sent

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,6 +1,37 @@
 import React from "react";
-
 const Contact = () => {
+  // const [toSend,setToSend] = useState({})
+  // const form = useRef();
+  // const handleForm = (event)=> {
+  //  event.preventDefault();
+  // //  const name = event.target.name.value;
+  // //  const email = event.target.email.value;
+  // //  const text = event.target.text.value;
+
+  // //  const info = {
+  // //    Name: name,
+  // //    Message: email,
+  // //    Email: text,
+  // //  };
+  // //  setToSend(info);
+
+  // emailjs
+  //   .sendForm(
+  //     "service_ghuwo95",
+  //     "template_n267ybb",
+  //     form.current,
+  //     "YOUR_PUBLIC_KEY"
+  //   )
+  //   .then(
+  //     (result) => {
+  //      alert(result.text);
+  //     },
+  //     (error) => {
+  //       alert(error.text);
+  //     }
+  //   );
+
+  // }
   return (
     <div id="contact">
       <div className="hero min-h-screen bg-base-200">
@@ -11,35 +42,50 @@ const Contact = () => {
             </h1>
             <div className="w-full">
               <div className="form-control">
-              <label className="label w-3/4 mx-auto">
-                  <span className="label-text  text-2xl ">Your Name</span>
-                </label>
-                <label className="input-group">
-                  <input
-                    type="text"
-                    placeholder="Your Name"
-                    className="input input-bordered w-3/4 mx-auto text-xl p-2"
-                  />
-                </label>
+                <form action="https://formspree.io/f/mbjerkpp" method="POST">
+                  <label className="label w-3/4 mx-auto">
+                    <span className="label-text  text-2xl ">Your Name</span>
+                  </label>
+                  <label className="input-group">
+                    <input
+                      type="text"
+                      name="name"
+                      placeholder="Your Name"
+                      className="input input-bordered w-3/4 mx-auto text-xl p-2"
+                    />
+                  </label>
 
-              <label className="label w-3/4 mx-auto">
-                  <span className="label-text text-2xl">Your Email</span>
-                </label>
-                <label className="input-group">
-                  <input
-                    type="email"
-                    placeholder="email@gmail.com"
-                    className="input input-bordered w-3/4 mx-auto text-xl"
-                  />
-                </label>
+                  <label className="label w-3/4 mx-auto">
+                    <span className="label-text text-2xl">Your Email</span>
+                  </label>
+                  <label className="input-group">
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="email@gmail.com"
+                      className="input input-bordered w-3/4 mx-auto text-xl"
+                    />
+                  </label>
 
-                <label className="label w-3/4 mx-auto">
-                  <span className="text-2xl">Your Message</span>
-                </label>
-                <label className="input-group">
-                <textarea className="textarea textarea-success w-3/4 mx-auto text-xl h-48" placeholder="Type Your message"></textarea>
-                </label>
-                <button className="btn btn-primary w-48 mx-auto mt-2">Send me</button>
+                  <label className="label w-3/4 mx-auto">
+                    <span className="text-2xl">Your Message</span>
+                  </label>
+                  <label className="input-group">
+                    <textarea
+                      name="text"
+                      className="textarea textarea-success w-3/4 mx-auto text-xl h-48"
+                      placeholder="Type Your message"
+                    ></textarea>
+                  </label>
+                  <div className="text-center">
+                    <button
+                      className="btn btn-primary w-48 mx-auto mt-2"
+                      type="submit"
+                    >
+                      Send me
+                    </button>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,21 +1,220 @@
 import React from "react";
 import jsfun from './js fun project.png';
+import chitchat from './Screenshot_1.png';
 const Project = () => {
   return (
     <div id="project">
       <h1 className="lg:text-5xl text-4xl font-bold text-center">Project</h1>
-      <div className="lg:flex justify-evenly w-3/4 mx-auto mt-4">
-        <div className="card card-compact w-96 bg-base-100 shadow-xl mx-auto">
-          <figure>
-            <img src={jsfun} alt="Shoes" />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title text-2xl">TicTacToe</h2>
-            <h3 className="text-xl">This is a simple JavaScript fun project.</h3>
-            <p className="text-xl"> <span className="font-bold">Using:</span> HTML CSS JavaScript  </p>
-            <div className="card-actions justify-evenly">
-              <button className="btn btn-primary">Live link</button>
-              <button className="btn btn-primary">Repository link</button>
+      <div className="flex justify-center w-full py-2 gap-2 mx-auto">
+        <a href="#FullStack" className="btn btn-xs">
+          Full Stack
+        </a>
+        <a href="#FunProject" className="btn btn-xs">
+          Fun Project
+        </a>
+      </div>
+      <div className="carousel w-full">
+        <div
+          id="FullStack"
+          className="carousel-item w-full grid grid-cols-1	lg:grid-cols-3"
+        >
+          <div className="chitchat lg:flex justify-evenly w-3/4 mx-auto mt-4">
+            <div className="card card-compact w-96 bg-base-100 shadow-xl mx-auto">
+              <figure>
+                <img src={chitchat} alt="Shoes" className="h-96	" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title text-2xl">ChitChat</h2>
+                <h3 className="text-xl">
+                  This is a Full Stack project. <br />
+                  Key Features: <br />
+                  * Login & Create account & password reset <br />* Show all
+                  friends and their posts and give like or comments <br />
+                  * Communicate friends by sending text <br />* Profile edit ,
+                  show user interaction(Like & comments)
+                </h3>
+                <p className="text-xl">
+                  {" "}
+                  <span className="font-bold">Using:</span> Bootstrap React
+                  Express MongoDB{" "}
+                </p>
+                <div className="card-actions justify-evenly">
+                  <button className="btn btn-primary">
+                    <a
+                      href="https://chitchat-771c4.web.app/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Live
+                    </a>
+                  </button>
+                  <button className="btn btn-primary">
+                    <a
+                      href="https://github.com/AkashChakrabortty/ChitChat"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Client
+                    </a>
+                  </button>
+                  <button className="btn btn-primary">
+                    <a
+                      href="https://github.com/AkashChakrabortty/ChitChat-server"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Server
+                    </a>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="Buy-Sell lg:flex justify-evenly w-3/4 mx-auto mt-4">
+            <div className="card card-compact w-96 bg-base-100 shadow-xl mx-auto">
+              <figure>
+                <img src={chitchat} alt="Shoes" className="h-96	" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title text-2xl">Buy-Sell</h2>
+                <h3 className="text-xl">
+                  This is a Full Stack project. <br />
+                  Key Features: <br />
+                  * Admin feature <br />
+                  * Seller feature
+                  <br />
+                  * Buyer feature <br />
+                  * Advertisement product <br />
+                  * Booking product <br />* Authentication
+                </h3>
+                <p className="text-xl">
+                  {" "}
+                  <span className="font-bold">Using:</span> Bootstrap React
+                  Express MongoDB{" "}
+                </p>
+                <div className="card-actions justify-evenly">
+                  <button className="btn btn-primary">
+                    <a
+                      href="https://buy-sell-6f50b.web.app/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Live
+                    </a>
+                  </button>
+                  <button className="btn btn-primary">
+                    <a
+                      href="https://github.com/AkashChakrabortty/Buy-Sell"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Client
+                    </a>
+                  </button>
+                  <button className="btn btn-primary">
+                    <a
+                      href="https://github.com/AkashChakrabortty/Buy-Sell-Server"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Server
+                    </a>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="Photographer lg:flex justify-evenly w-3/4 mx-auto mt-4">
+            <div className="card card-compact w-96 bg-base-100 shadow-xl mx-auto">
+              <figure>
+                <img src={chitchat} alt="Shoes" className="h-96	" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title text-2xl">Photographer</h2>
+                <h3 className="text-xl">
+                  This is a Full Stack project. <br />
+                  Key Features: <br />
+                  * Gmail and Email password login <br />
+                  * Give review <br />
+                  * Show others user review <br />
+                  * Add review <br />* Edit & Delete review
+                </h3>
+                <p className="text-xl">
+                  {" "}
+                  <span className="font-bold">Using:</span> Bootstrap React
+                  Express MongoDB{" "}
+                </p>
+                <div className="card-actions justify-evenly">
+                  <button className="btn btn-primary">
+                    <a
+                      href="https://photographer1-6b6ff.web.app/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Live
+                    </a>
+                  </button>
+                  <button className="btn btn-primary">
+                    <a
+                      href="https://github.com/AkashChakrabortty/Photographer-client"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Client
+                    </a>
+                  </button>
+                  <button className="btn btn-primary">
+                    <a
+                      href="https://github.com/AkashChakrabortty/Photographer-Server"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Server
+                    </a>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="FunProject" className="carousel-item w-full">
+          <div className="lg:flex justify-evenly w-3/4 mx-auto mt-4">
+            <div className="card card-compact w-96 bg-base-100 shadow-xl mx-auto">
+              <figure>
+                <img src={jsfun} alt="Shoes" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title text-2xl">TicTacToe</h2>
+                <h3 className="text-xl">
+                  This is a simple JavaScript fun project.
+                </h3>
+                <p className="text-xl">
+                  {" "}
+                  <span className="font-bold">Using:</span> HTML CSS JavaScript{" "}
+                </p>
+                <div className="card-actions justify-evenly">
+                  <button className="btn btn-primary">
+                    <a
+                      href="https://akashchakrabortty.github.io/TicTacToe/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Live link
+                    </a>
+                  </button>
+                  <button className="btn btn-primary">
+                    <a
+                      href="https://github.com/AkashChakrabortty/TicTacToe"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Repository link
+                    </a>
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
